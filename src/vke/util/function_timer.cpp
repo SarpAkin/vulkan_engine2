90cd300c9e597a2e51ef0a0451b9ee25b349f2c6
SarpAkin <sarp4.akin@gmail.com>
2023-09-29 21:09:45 +0300

    function tier now takes the avarage of function call

--- src/vke/util/function_timer.cpp
+++ src/vke/util/function_timer.cpp
@@ -1,6 +1,33 @@
 #include "function_timer.hpp"
 
 #include <fmt/core.h>
+#include <mutex>
+#include <unordered_map>
+
+class TimingManager {
+public:
+    void function_time(const char* function, double time) {
+        std::lock_guard<std::mutex> guard(m_lock);
+
+        m_stats[function].total_time += time;
+        m_stats[function].invocation_count += 1;
+    }
+
+    ~TimingManager() {
+        for (auto& [fname, stats] : m_stats) {
+            printf("[Function Timer] function: %s took %.4f ms on avarage to run. invocation count = %ld \n", fname, stats.total_time / stats.invocation_count, stats.invocation_count);
+        }
+    }
+
+    struct FunctionStats {
+        double total_time         = 0.0;
+        uint64_t invocation_count = 0;
+    };
+
+private:
+    std::mutex m_lock;
+    std::unordered_map<const char*, FunctionStats> m_stats;
+} man;
 
 namespace vke {
 namespace impl {
@@ -11,7 +38,8 @@ FunctionTimer::FunctionTimer(const char* func_name) {
 
 FunctionTimer ::~FunctionTimer() {
     double elapsed_time = ((double)(std::chrono::steady_clock::now() - m_start).count()) / 1000000.0;
-    fmt::print("[Function Timer] function: {} took {} ms to run\n", m_func_name, elapsed_time);
+    man.function_time(m_func_name, elapsed_time);
+    // fmt::print("[Function Timer] function: {} took {} ms to run\n", m_func_name, elapsed_time);
 }
 } // namespace impl
 
