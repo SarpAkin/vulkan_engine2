a6cfce31be640265b317a18ce0c59f794a17443b
SarpAkin <sarp4.akin@gmail.com>
2023-11-25 15:05:56 +0300

    added property of render type to render targets

--- src/vke/engine/render_target.hpp
+++ src/vke/engine/render_target.hpp
@@ -7,11 +7,12 @@ namespace vke {
 
 class RPRenderTarget : public IRenderTarget, public vke::System<struct RPRenderTargetFramelyData> {
 public:
-    RPRenderTarget(RenderEngine* engine, Renderpass* rp, u32 subpass_index);
+    RPRenderTarget(RenderEngine* engine, Renderpass* rp, u32 subpass_index, RenderTargetType type = RenderTargetType::UNDEFINED);
     ~RPRenderTarget();
 
-    void set_camera(Camera* camera)override { m_camera = camera; }
+    void set_camera(Camera* camera) override { m_camera = camera; }
 
+    RenderTargetType get_render_target_type() const override { return m_type; }
     Renderpass* get_renderpass() override { return m_render_pass; }
     u32 get_subpass_index() override { return m_subpass_index; }
     Camera* get_camera() override { return m_camera; }
@@ -19,14 +20,17 @@ public:
     CommandBuffer* get_draw_cmd() override;
     CommandBuffer* get_compute_cmd() override;
 
-    void subscribe(IRenderSystem* system) override;
-    void render_systems() override;
+    void subscribe(IRenderSystem* system) final override;
+    void render_systems() final override;
 
-private:
+
+protected:
     Renderpass* m_render_pass;
     u32 m_subpass_index;
+    RenderTargetType m_type;
     Camera* m_camera;
 
+private:
     std::vector<IRenderSystem*> m_systems;
 };
 
