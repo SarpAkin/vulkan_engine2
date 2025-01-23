cf51516b0a554c1b11416eda8ee3cd7f66e9272c
SarpAkin <sarp4.akin@gmail.com>
2025-01-23 14:03:41 +0300

    added utility functions for transform

--- src/scene/components/transform.hpp
+++ src/scene/components/transform.hpp
@@ -1,21 +1,21 @@
 #pragma once
 
-#include <glm/vec3.hpp>
-#include <glm/mat4x4.hpp>
 #include <glm/gtc/quaternion.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/vec3.hpp>
 
-namespace vke{
+namespace vke {
 
-struct Transform{
+void decompose_matrix(const glm::mat4& matrix, glm::vec3& translation, glm::quat& rotation, glm::vec3& scale);
+
+struct Transform {
     glm::dvec3 position;
     glm::quat rotation;
     glm::vec3 scale;
 
-    glm::mat4 local_model_matrix(const glm::dvec3& pivot_position = {}){
-        auto rel_position = glm::vec3(position - pivot_position);
-        return glm::translate(glm::mat4(1), rel_position);
-    }
+    glm::mat4 local_model_matrix(const glm::dvec3& pivot_position = {});
+
+    static Transform decompose_from_matrix(const glm::mat4& mat);
 };
 
-
-}
+} // namespace vke
