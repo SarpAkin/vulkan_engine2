2aa43b790060fd762d58a9ae6f283762036c5f75
SarpAkin <sarp4.akin@gmail.com>
2025-01-25 15:41:21 +0300

    texture coord support for mesh

--- src/render/mesh/mesh.cpp
+++ src/render/mesh/mesh.cpp
@@ -6,24 +6,6 @@
 
 namespace vke {
 
-std::unique_ptr<Mesh> Mesh::make_mesh(std::span<Vertex> verticies, std::span<uint16_t> indicies) {
-
-    auto vb = std::make_unique<vke::Buffer>(VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, verticies.size_bytes(), true);
-    memcpy(vb->mapped_data_bytes().data(), verticies.data(), verticies.size_bytes());
-
-    auto ib = std::make_unique<vke::Buffer>(VK_BUFFER_USAGE_INDEX_BUFFER_BIT, indicies.size_bytes(), true);
-    memcpy(ib->mapped_data_bytes().data(), indicies.data(), indicies.size_bytes());
-
-    auto mesh = std::make_unique<Mesh>();
-    ;
-    mesh->vertex_buffer = std::move(vb);
-    mesh->index_buffer  = std::move(ib);
-    mesh->index_count   = indicies.size();
-    mesh->index_type    = VK_INDEX_TYPE_UINT16;
-
-    return mesh;
-}
-
 Mesh::~Mesh() {}
 void MeshBuilder::set_indicies(std::span<uint16_t> span) {
     m_indicies_in_bytes = vke::span_cast<uint8_t>(span);
@@ -43,10 +25,14 @@ Mesh MeshBuilder::build(vke::CommandBuffer* buffer) const {
     // total_byte_size += m_indicies_in_bytes.size_bytes();
 
     Mesh mesh;
+    auto add_buffer = [&]<class T>(std::span<T> data) {
+        auto buffer = std::make_unique<vke::Buffer>(VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, data.size_bytes(), true);
+        memcpy(buffer->mapped_data_bytes().data(), data.data(), data.size_bytes());
+        mesh.vertex_buffers.push_back(std::move(buffer));
+    };
 
-    auto vb = std::make_unique<vke::Buffer>(VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, m_positions.size_bytes(), true);
-    memcpy(vb->mapped_data_bytes().data(), m_positions.data(), m_positions.size_bytes());
-    mesh.vertex_buffer = std::move(vb);
+    add_buffer(m_positions);
+    add_buffer(m_texture_coords);
 
     if (m_indicies_in_bytes.size() > 0) {
         auto ib = std::make_unique<vke::Buffer>(VK_BUFFER_USAGE_INDEX_BUFFER_BIT, m_indicies_in_bytes.size_bytes(), true);
--- src/render/mesh/mesh.hpp
+++ src/render/mesh/mesh.hpp
@@ -16,12 +16,12 @@ struct Mesh {
         glm::vec3 pos;
         u32 color;
     };
-    std::unique_ptr<vke::IBuffer> vertex_buffer, index_buffer;
+    std::unique_ptr<vke::IBufferSpan> index_buffer;
+    std::vector<std::unique_ptr<vke::IBufferSpan>> vertex_buffers;
     VkIndexType index_type = VK_INDEX_TYPE_NONE_KHR;
     uint32_t index_count; // if index_type is VK_INDEX_TYPE_NONE than it means vertex_count
 
 public:
-    static std::unique_ptr<Mesh> make_mesh(std::span<Vertex> verticies, std::span<uint16_t> indicies);
 
     Mesh()                                 = default;
     Mesh& operator=(Mesh&& other) noexcept = default;
@@ -32,6 +32,8 @@ public:
 class MeshBuilder {
 public:
     void set_positions(std::span<glm::vec3> span) { m_positions = span; }
+    void set_texture_coords(std::span<glm::vec2> span) { m_texture_coords = span; }
+
     void set_indicies(std::span<uint16_t> span);
     void set_indicies(std::span<uint32_t> span);
 
