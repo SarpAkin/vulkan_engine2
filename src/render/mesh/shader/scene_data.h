635e7c1c25a8ac5a4f7d42c3ba44e11a1f7ddea7
SarpAkin <sarp4.akin@gmail.com>
2025-03-01 14:19:15 +0300

    added new set bindings for indirect rendering

--- src/render/mesh/shader/scene_data.h
+++ src/render/mesh/shader/scene_data.h
@@ -2,24 +2,38 @@
 #define SCENE_DATA_H
 
 #ifdef __cplusplus
+#include <glm/mat4x4.hpp>
 #include <glm/vec2.hpp>
 #include <glm/vec3.hpp>
 #include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
 
 using namespace glm;
-#else 
+#else
 #extension GL_ARB_gpu_shader_int64 : enable
 
 #endif
 
-struct ViewData{
+// copy of VkDrawIndexedIndirectCommand defined in "vulkan.h"
+struct VkDrawIndexedIndirectCommand_ {
+    uint indexCount;
+    uint instanceCount;
+    uint firstIndex;
+    int vertexOffset;
+    uint firstInstance;
+};
+
+struct AABB {
+    vec4 start;
+    vec4 size;
+};
+
+struct ViewData {
     mat4 proj_view;
     mat4 inv_proj_view;
     dvec4 view_world_pos;
 };
 
-struct MaterialData{
+struct MaterialData {
     uint texture_ids[4];
     float roughness;
     float specular;
@@ -27,7 +41,7 @@ struct MaterialData{
     float padd;
 };
 
-struct InstanceData{
+struct InstanceData {
     dvec4 world_position;
     vec4 size;
     vec4 rotation;
@@ -35,36 +49,49 @@ struct InstanceData{
     uint padd[3];
 };
 
-struct PartData{
-    uint64_t component_addresses[4];
+#define MAX_PARTS 32
+
+struct PartData {
+    uint mesh_id;
+    uint material_id;
 };
 
-struct ModelData{
+struct ModelData {
     vec4 aabb_size;
+    uint part_index;
+    uint part_count;
+    uint padd[2];
+};
 
+struct MeshData {
+    uint64_t vertex_pointers[3];
+    uint index_offset;
+    uint index_count;
+};
+
+struct InstanceDrawParameter{
+    mat4 model_matrix;
 };
 
 #define MAX_LIGHTS 15
 
-struct PointLight{
+struct PointLight {
     vec4 color;
     vec3 pos;
     float range;
 };
 
-struct DirectionalLight{
+struct DirectionalLight {
     vec4 dir;
     vec4 color;
 };
 
-//requires std430
-struct SceneLightData{
+// requires std430
+struct SceneLightData {
     PointLight point_lights[MAX_LIGHTS];
     DirectionalLight directional_light;
     vec4 ambient_light;
     uint point_light_count;
 };
-
-
 
 #endif
--- src/render/mesh/shader/scene_set.glsl
+++ src/render/mesh/shader/scene_set.glsl
@@ -8,14 +8,58 @@
 #define SCENE_SET 0
 #define VIEW_SET 1
 
-layout(set = SCENE_SET, binding = 0,std430) readonly buffer Buffer {
+layout(set = SCENE_SET, binding = 0, std430) readonly buffer BufferS0 {
     SceneLightData lights;
 };
 
+layout(set = SCENE_SET, binding = 1, std430) readonly buffer BufferS1 {
+    InstanceData instances[];
+};
 
-layout(set = VIEW_SET,binding = 0,std430) uniform Buffer1{
+layout(set = SCENE_SET, binding = 2, std430) readonly buffer BufferS2 {
+    ModelData models[];
+};
+
+layout(set = SCENE_SET, binding = 3, std430)readonly buffer BufferS3 {
+    PartData parts[];
+};
+
+layout(set = SCENE_SET,binding 4,std430) readonly buffer BufferS4{
+    MeshData mesh[];
+}
+
+#ifdef COMPUTE
+
+// layout(set = SCENE_SET,binding )
+
+#endif
+
+layout(set = VIEW_SET, binding = 0, std430) uniform BufferV0 {
     ViewData scene_view;
 };
 
+layout(set = VIEW_SET, binding = 1, std430) readonly buffer BufferV1 {
+    //indexes correspond to partIDs
+    uvec2 instance_draw_parameter_locations[];
+};
+
+layout(set = VIEW_SET, binding = 2, std430) buffer BufferV2 {
+    //indexes correspond to partIDs
+    uint instance_counters[];
+};
+
+layout(set = VIEW_SET, binding = 3, std430) readonly buffer BufferV3 {
+    //indexes correspond to partIDs
+    uint indirect_draw_locations[];
+};
+
+layout(set = VIEW_SET, binding = 4, std430) buffer BufferV4 {
+    //indexes should come from indirect_draw_locations[partID]
+    VkDrawIndexedIndirectCommand_ draw_commands[];
+};
+
+layout(set = VIEW_SET, binding = 5, std430) buffer BufferV5 {
+    InstanceDrawParameter instance_draw_parameters[];
+};
 
 #endif
