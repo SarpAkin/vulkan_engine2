ee7b08bdb5ba210ad3492cebd7b13eed3724ad4a
SarpAkin <sarp4.akin@gmail.com>
2025-06-27 11:52:06 +0300

    removed entt::registry from scene

--- src/scene/scene.hpp
+++ src/scene/scene.hpp
@@ -13,13 +13,11 @@ public:
     Scene() {};
     ~Scene() {}
 
-    entt::registry* get_registry() { return &m_registry; }
     flecs::world* get_world() { return &world; }
     
     Camera* get_camera() { return &m_camera; }
 
 private:
-    entt::registry m_registry;
     flecs::world world;
 
     FreeCamera m_camera;
