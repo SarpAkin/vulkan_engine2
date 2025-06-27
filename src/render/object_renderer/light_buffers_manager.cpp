1d78e64ecadaeb2eb47ec9bc9fef140c27db26e5
SarpAkin <sarp4.akin@gmail.com>
2025-06-27 11:50:20 +0300

    light buffer manager now uses the new gpu handles manager and flecs

--- src/render/object_renderer/light_buffers_manager.cpp
+++ src/render/object_renderer/light_buffers_manager.cpp
@@ -13,78 +13,38 @@ struct CPointLightInstance {
     const LightBuffersManager::LightID id;
 };
 
-LightBuffersManager::LightBuffersManager(vke::RenderServer* render_server, entt::registry* registry) {
-    m_registry = registry = nullptr;
+LightBuffersManager::LightBuffersManager(vke::RenderServer* render_server, flecs::world* world) {
+    m_world = world;
     m_render_server       = render_server;
     m_light_buffer        = std::make_unique<vke::Buffer>(
         VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
         sizeof(SceneLightData) + sizeof(PointLight) * m_max_point_light_count, false //
     );
 
-    // connect_registry_callbacks();
-
     m_shadow_manager = std::make_unique<ShadowManager>(m_render_server);
+    m_light_handle_manager = std::make_unique<GPUHandleIDManager<CPointLight>>(world);
 }
 
 LightBuffersManager::~LightBuffersManager() {
-    // disconnect_registry_callbacks();
-}
-
-void LightBuffersManager::connect_registry_callbacks() {
-    m_registry->on_construct<CPointLight>().connect<&LightBuffersManager::renderable_component_creation_callback>(this);
-    m_registry->on_update<CPointLight>().connect<&LightBuffersManager::renderable_component_update_callback>(this);
-    m_registry->on_destroy<CPointLight>().connect<&LightBuffersManager::renderable_component_destroy_callback>(this);
-}
-
-void LightBuffersManager::disconnect_registry_callbacks() {
-    m_registry->on_construct<CPointLight>().disconnect(this);
-    m_registry->on_update<CPointLight>().disconnect(this);
-    m_registry->on_destroy<CPointLight>().disconnect(this);
-}
-
-void LightBuffersManager::renderable_component_creation_callback(entt::registry& r, entt::entity e) {
-    m_pending_entities_for_register.push_back(e);
-}
-
-void LightBuffersManager::renderable_component_update_callback(entt::registry&, entt::entity e) {
-    printf("entity %d's renderable component has been modified\n", e);
-    TODO();
-}
-
-void LightBuffersManager::renderable_component_destroy_callback(entt::registry& r, entt::entity e) {
-    auto instance = r.try_get<CPointLightInstance>(e);
-    if (instance == nullptr) return;
-
-    m_pending_instances_for_destruction.push_back(instance->id);
-
-    // m_pending_instances_for_destruction.push_back(instance->id);
 }
 
 void LightBuffersManager::flush_pending_lights(vke::CommandBuffer& cmd) {
-    return;
     StencilBuffer stencil;
-
-    auto view = m_registry->view<Transform, CPointLight>();
 
     auto point_lights = m_light_buffer->subspan(sizeof(SceneLightData));
 
-    for (auto e : m_pending_entities_for_register) {
-        auto id = m_light_id_manager.new_id();
-
-        m_registry->emplace<CPointLightInstance>(e, CPointLightInstance{.id = id});
-
-        auto [t, l] = view.get(e);
+    m_light_handle_manager->flush_and_register_handles([&](flecs::entity e, LightID id) {
+        auto t = e.get<Transform>();
+        auto l = e.get<CPointLight>();
 
         PointLight light{
-            .color = glm::vec4(l.color, 0.0),
-            .pos   = glm::vec3(t.position),
-            .range = l.range,
+            .color = glm::vec4(l->color, 0.0),
+            .pos   = glm::vec3(t->position),
+            .range = l->range,
         };
 
         stencil.copy_data(point_lights.subspan_item<PointLight>(id.id, 1), &light, 1);
-    }
-
-    m_pending_entities_for_register.clear();
+    });
 
     glm::vec3 directional_light_dir = glm::normalize(glm::vec3(1, -1, 1));
 
@@ -105,7 +65,7 @@ void LightBuffersManager::flush_pending_lights(vke::CommandBuffer& cmd) {
             // .proj_view = m_shadow_manager->get_direct_shadow_map(0)->get_projection_view_matrix(),
         },
         .ambient_light     = glm::vec4(0.04, 0.03, 0.03, 0.0),
-        .point_light_count = static_cast<u32>(m_light_id_manager.id_manager.id_count()),
+        .point_light_count = static_cast<u32>(m_light_handle_manager->get_id_manager().id_manager.id_count()),
     };
 
     auto& min_zs = m_shadow_manager->get_min_zs_for_direct_shadow_maps(0);
--- src/render/object_renderer/light_buffers_manager.hpp
+++ src/render/object_renderer/light_buffers_manager.hpp
@@ -8,41 +8,34 @@
 
 #include "fwd.hpp"
 #include "render/iobject_renderer.hpp"
+#include "generic_entity_gpu_handle_manager.hpp"
+
 
 namespace vke {
 
 class LightBuffersManager {
 public: // c'tor & d'tor
-    LightBuffersManager(vke::RenderServer* render_server,entt::registry* registry);
+    LightBuffersManager(vke::RenderServer* render_server,flecs::world* world);
     ~LightBuffersManager();
 
 public: // getters
 public:
-    using LightID = impl::GenericID<CPointLight>;
+    using LightID = GPUHandleIDManager<CPointLight>::HandleID;
     void flush_pending_lights(vke::CommandBuffer& cmd);
 
     IBuffer* get_get_lights_buffer() const { return m_light_buffer.get(); }
     vke::ShadowManager* get_shadow_manager() { return m_shadow_manager.get(); }
 
 private:
-    void connect_registry_callbacks();
-    void disconnect_registry_callbacks();
-
-    void renderable_component_creation_callback(entt::registry&, entt::entity e);
-    void renderable_component_update_callback(entt::registry&, entt::entity e);
-    void renderable_component_destroy_callback(entt::registry&, entt::entity e);
-
-private:
     std::unique_ptr<vke::Buffer> m_light_buffer;
     std::unordered_map<std::string, std::unique_ptr<vke::Buffer>> m_light_tile_buffers;
-    entt::registry* m_registry;
+    flecs::world* m_world;
     vke::RenderServer* m_render_server;
     std::unique_ptr<vke::ShadowManager> m_shadow_manager;
 
-    std::vector<entt::entity> m_pending_entities_for_register;
-    std::vector<LightID> m_pending_instances_for_destruction;
 
-    GenericIDManager<LightID> m_light_id_manager = GenericIDManager<LightID>(0);
+    std::unique_ptr<GPUHandleIDManager<CPointLight>> m_light_handle_manager;
+    
 
     u32 m_max_point_light_count = 1020;
 };
