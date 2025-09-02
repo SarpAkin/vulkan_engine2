603ece3beb3b7759f910cefb700aa30b59866057
SarpAkin <sarp4.akin@gmail.com>
2025-09-02 12:17:28 +0300

    added precompiled headers

---
+++ pch/pch.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <any>           // IWYU pragma: export
+#include <atomic>        // IWYU pragma: export
+#include <chrono>        // IWYU pragma: export
+#include <deque>         // IWYU pragma: export
+#include <exception>     // IWYU pragma: export
+#include <format>        // IWYU pragma: export
+#include <functional>    // IWYU pragma: export
+#include <iostream>      // IWYU pragma: export
+#include <iterator>      // IWYU pragma: export
+#include <memory>        // IWYU pragma: export
+#include <mutex>         // IWYU pragma: export
+#include <numbers>       // IWYU pragma: export
+#include <optional>      // IWYU pragma: export
+#include <ranges>        // IWYU pragma: export
+#include <span>          // IWYU pragma: export
+#include <string>        // IWYU pragma: export
+#include <string_view>   // IWYU pragma: export
+#include <tuple>         // IWYU pragma: export
+#include <unordered_map> // IWYU pragma: export
+#include <unordered_set> // IWYU pragma: export
+#include <variant>       // IWYU pragma: export
+#include <vector>        // IWYU pragma: export
+
+#include <cassert>   // IWYU pragma: export
+#include <cinttypes> // IWYU pragma: export
+#include <cmath>     // IWYU pragma: export
+#include <cstdio>    // IWYU pragma: export
+
+#include <vulkan/vulkan.h>   // IWYU pragma: export
+#include <vulkan/vulkan.hpp> // IWYU pragma: export
+
+#include <vk_mem_alloc.h> // IWYU pragma: export
+
+#include <glm/glm.hpp> // IWYU pragma: export
+
+#include <flecs.h>
+#include <flecs/addons/flecs_c.h>   // IWYU pragma: export
+#include <flecs/addons/flecs_cpp.h> // IWYU pragma: export
+
+#include <vke/fwd.hpp>          // IWYU pragma: export
+#include <vke/util.hpp>         // IWYU pragma: export
+#include <vke/vke.hpp>          // IWYU pragma: export
+#include <vke/vke_builders.hpp> // IWYU pragma: export
