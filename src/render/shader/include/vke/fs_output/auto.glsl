30cf621d3e164a0bad88a37ce602599d6b0cf07b
SarpAkin <sarp4.akin@gmail.com>
2025-09-29 12:03:09 +0300

    fixed auto fragment shader output

--- src/render/shader/include/vke/fs_output/auto.glsl
+++ src/render/shader/include/vke/fs_output/auto.glsl
@@ -1,9 +1,9 @@
 #ifndef VKE_FS_OUT_AUTO
 #define VKE_FS_OUT_AUTO
 
-#ifdef GPASS
+#if defined(GPASS)
 #include "gpass.glsl"
-#elif SHADOW_PASS
+#elif defined(SHADOW_PASS)
 #include "shadow.glsl"
 #else
 #include "default.glsl"
